4e5c31aa1885c687129ace183f7fff7ac382234d
Emirmir47 <emirmir457@gmail.com>
2024-02-05 18:42:39 +0200

    New controller: CategoriesController

---
+++ Controllers/CategoriesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShumenNews.Models.ViewModels;
+using ShumenNews.Services;
+
+namespace ShumenNews.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly ICategoryService categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            this.categoryService = categoryService;
+        }
+        public IActionResult Index(string id)
+        {
+            var category = categoryService.GetCategoryById(id);
+            var model = new CategoryViewModel
+            {
+                Name = category.Name,
+            };
+            return View(model);
+        }
+    }
+}
---
+++ Views/Categories/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ShumenNews.Views.Categories
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
